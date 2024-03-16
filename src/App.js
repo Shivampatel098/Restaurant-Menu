62d935b4c6296eed8123462da133cfc3b05cb5ad
Shivampatel098 <132866344+Shivampatel098@users.noreply.github.com>
2024-03-16 15:16:06 -0400

    Edited routes for deployement

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import UserForm from './component/Hooks/UseState';
 import UseEffect from './component/Hooks/UseEffect';
 import LoginSignup from './component/Basics/LoginSignup';
 
-/* const App = () => {
-  return (
-      <div>
-        <p>Welcome <MyName />, to The world of React App</p>
-      </div>
-      // <p> This is not allowed in React, while we use JSX, it should be enclosed in the upper div tag. </p>
-  );
-};
-const MyName = () => {
-  return <h1>Shivam Patel</h1>;
-  
-};
 
-export default App
-*/
 // In this app.js file we write components.
 
 
@@ -30,9 +16,6 @@ const App = () => {
     <Routes>
       {/* <Route path='/' element={<LoginSignup />}></Route> */}
       <Route path='/' element={<Restaurant />}></Route>
-      <Route path='/LoginSignup' element={<LoginSignup />}></Route>
-      <Route path='/CheckAge' element={<UserForm />}></Route>
-      <Route path='/ChatNumber' element={<UseEffect />}></Route>
 
     </Routes>
   </BrowserRouter>
